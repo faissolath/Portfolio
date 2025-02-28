1150bedbd0b5c8a578ece2cdeb791ac4640524c6
ADEOTI Faissolath <adeotifaissolath29@gmail.com>
2025-02-28 20:16:42 +0100

    Modification de index javaScript css et de photo

--- script.js
+++ script.js
@@ -37,7 +37,59 @@ document.addEventListener("DOMContentLoaded", () => {
         observer.observe(aboutSection);
     }
 
-    animateAbout(); // Appeler la fonction pour observer la section "À propos"
+    animateAbout(); // Appeler la fonction pour observer la section "A propos"
+
+    
+    // Animation pour la section "Competences"
+    const skillsSection = document.getElementById('skills');
+
+    function animateSkills() {
+        const observer = new IntersectionObserver((entries) => {
+            entries.forEach((entry) => {
+                if (entry.isIntersecting) {
+                    skillsSection.classList.add('visible'); // Ajouter la classe "visible" pour déclencher l'animation
+                }
+            });
+        }, { threshold: 0.5 }); // Déclencher lorsque 50% de la section est visible
+
+        observer.observe(skillsSection);
+    }
+
+    animateSkills();// Appeler la fonction au chargement de la page
+
+    // Animation pour la section "Services"
+    const servicesSection = document.getElementById('services');
+
+    function animateServices() {
+        const observer = new IntersectionObserver((entries) => {
+            entries.forEach((entry) => {
+                if (entry.isIntersecting) {
+                    servicesSection.classList.add('visible'); // Ajouter la classe "visible" pour déclencher l'animation
+                }
+            });
+        }, { threshold: 0.5 }); // Déclencher lorsque 50% de la section est visible
+
+        observer.observe(servicesSection);
+    }
+
+    animateServices();// Appeler la fonction au chargement de la page
+
+    // Animation pour la section "Cv-Contact"
+    const cv_contactSection = document.getElementById('cv_contact');
+
+    function animateCv_contact() {
+        const observer = new IntersectionObserver((entries) => {
+            entries.forEach((entry) => {
+                if (entry.isIntersecting) {
+                    cv_contactSection.classList.add('visible'); // Ajouter la classe "visible" pour déclencher l'animation
+                }
+            });
+        }, { threshold: 0.5 }); // Déclencher lorsque 50% de la section est visible
+
+        observer.observe(cv_contactSection);
+    }
+
+    animateCv_contact();// Appeler la fonction au chargement de la page
 
     // Animation pour la section "Formation" (si nécessaire)
     const formationSection = document.getElementById("formation");
